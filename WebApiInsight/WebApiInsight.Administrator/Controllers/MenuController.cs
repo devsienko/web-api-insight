d9cdf53a94d1a7d0579bf3fb0658f8f7130a4dc6
daniil_ <daniil_evsienko@mail.ru>
2018-06-05 18:17:02 +0400

    Improved agent settings functionality (added agents count in the top menu)

---
+++ WebApiInsight/WebApiInsight.Administrator/Controllers/MenuController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace WebApiInsight.Administrator.Controllers
+{
+    [Authorize]
+    public class MenuController : Controller
+    {
+        public PartialViewResult Index()
+        {
+            ViewBag.AgentsCount = new AgentsManager().GetAgents().Count;
+            return PartialView();
+        }
+    }
+}
